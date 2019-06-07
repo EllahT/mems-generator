1dd1625e48a2761b6c017096305f3e66176802eb
Ellah Toren <ellahtoren@gmail.com>
2019-06-07 20:35:45 +0300

    control box changes

--- js/util-service.js
+++ js/util-service.js
@@ -1,10 +1,10 @@
 'use strict'
 
-function makeId(length=5) {
+function makeId(length = 5) {
     var txt = '';
     var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-    for(var i=0; i < length; i++) {
+    for (var i = 0; i < length; i++) {
         txt += possible.charAt(Math.floor(Math.random() * possible.length));
     }
     return txt;
@@ -19,12 +19,22 @@ function loadFromStorage(key) {
     return JSON.parse(localStorage.getItem(key))
 }
 
-function onAddClassToEl(className,newClass) {
-    const el = document.querySelector('.'+className);
+function onAddClassToEl(className, newClass) {
+    const el = document.querySelector('.' + className);
     el.classList.add(newClass);
 }
 
-function onRemoveClassToEl(className,removedClass) {
-    const el = document.querySelector('.'+className);
+function onRemoveClassToEl(className, removedClass) {
+    const el = document.querySelector('.' + className);
     el.classList.remove(removedClass);
+}
+
+function onToggleClassToEl(className, toggeledClass) {
+    const el = document.querySelector('.' + className);
+    el.classList.toggle(toggeledClass);
+}
+
+function onToggleHide(className) {
+    const el = document.querySelector('.' + className);
+    el.classList.toggle('hide');
 }
